b0ce696f0afa2dbe46a4d502fdfe341029874ab2
RollingHog <46373241+RollingHog@users.noreply.github.com>
2024-01-08 08:17:55 +0300

    catch-the-star initial

--- catch-the-star/index.js
+++ catch-the-star/index.js
@@ -11,6 +11,18 @@ function getEl(name) {
   return document.getElementById(name)
 }
 
+// thx https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+// eslint-disable-next-line no-unused-vars
+const sleep = (timeout) => new Promise(r => setTimeout(r, timeout))
+
+
+// eslint-disable-next-line no-unused-vars
 function createElementFromHTML(htmlString) {
   const div = document.createElement('div')
   div.innerHTML = htmlString.trim()
@@ -19,8 +31,78 @@ function createElementFromHTML(htmlString) {
 
 ////////////////////// MAIN
 
+const data = {
+  _points: 0,
+  set points(value) {
+    this._points += +value
+    getEl('stat_points').innerText = this._points
+  },
+  _lost: 0,
+  lost() {
+    this._lost += 1
+    getEl('stat_lost').innerText = this._lost
+  }
+}
+
+// should be same as star width
+const STAR_PADDING = 60
+
+function addStar() {
+  const star = document.createElement('img')
+  star.classList.add('star')
+  star.draggable = false
+  star.setAttribute('src', 'star.png')
+  star.style.top = '0px'
+  star.style.left = `${getRandomInt(STAR_PADDING, window.innerWidth-STAR_PADDING)}px`
+  star.onclick = () => {
+    data.points = 1
+    star.remove()
+  }
+
+  getEl('starsWrap').appendChild(star)
+}
+
+let difficultyMult = 0.08
+
+let mainSpeed = 3
+
+async function mainCycle() {
+  while(true) {
+    /**
+     * @type {HTMLElement[]}
+    */
+   let stars = document.querySelectorAll('img.star')
+   getEl('stat_stars').innerText = stars.length
+    //  log('stars:', stars.length)
+
+    if(Math.random() < difficultyMult) {
+      addStar()
+    }
+    if(stars.length < 5) {
+        if(Math.random() < difficultyMult) {
+        addStar()
+      }
+    }
+
+
+    for(let star of stars) {
+      const tCoord = parseInt(star.style.top)
+      if (tCoord > window.innerHeight) {
+        data.lost()
+        star.remove()
+        continue
+      }
+      let speed = mainSpeed
+      star.style.top = `${tCoord + speed}px`
+    }
+
+    await sleep(50)
+  }
+}
+
 function init() {
   // getEl('el__version').innerText = VERSION
+  mainCycle()
 }
 
 try {
